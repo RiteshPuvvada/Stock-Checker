623fa5bf27921838f60b4690fe0bee4b88445344
RiteshPuvvada <riteshpuvvada@gmail.com>
2021-07-23 16:03:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+<img width="100px" alt="StockPrice logo" src="public/icons/android-chrome-512x512.png">
+<br>
+<h2>Stock Price Checker 🚀</h2>
+</div>
---
+++ server.js
@@ -0,0 +1,67 @@
+'use strict';
+require('dotenv').config();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+require("./db-connection")
+
+let helmet = require("helmet")
+
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//content security policies
+app.use(helmet.contentSecurityPolicy({
+  directives : {
+    scriptSrc : ["'self'"],
+    styleSrc : ["'self'"]
+  }
+}))
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);  
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+//Start our server and tests!
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 3500);
+  }
+});
+
+module.exports = app; //for testing
